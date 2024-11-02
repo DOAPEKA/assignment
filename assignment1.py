c6150e377b7889a9df8d81a90f63344de66b4acd
DOAPEKA <80774106+DOAPEKA@users.noreply.github.com>
2024-11-02 21:41:44 +0800

    Update assignment1.py

--- assignment1.py
+++ assignment1.py
@@ -1,21 +1,19 @@
-def draw_python():
-    import turtle as t
-    t.setup(800, 300, 200, 200)
-    t.penup()
-    t.fd(-200)
-    t.pendown()
-    t.seth(-50)
-    t.pensize(25)
-    t.pencolor("blue")
-    for i in range(4):
-        t.circle(5, 100)
-        t.circle(-100, 50)
-    t.circle(50, 100 / 2)
-    t.fd(50)
-    t.circle(20, 200)
-    t.fd(50 * 2 / 3)
-    t.done()
-
-
 print("----- Welcome to the drawing system ----")
 draw_python()
+
+import turtle as t
+
+def draw_star():
+    t.setup(800, 600)
+    t.speed(2)
+    t.penup()
+    t.goto(0, -100)
+    t.pendown()
+    t.pensize(2)
+    t.pencolor("red")
+    for i in range(5):
+        t.forward(200)
+        t.right(144)
+    t.done()
+
+draw_star()
