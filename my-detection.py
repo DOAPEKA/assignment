0cf0c6dd19ef9e40aa3d7eac3d664efbfcb02251
DOAPEKA <80774106+DOAPEKA@users.noreply.github.com>
2024-11-16 12:30:46 +0800

    Add files via upload

---
+++ my-detection.py
@@ -0,0 +1,22 @@
+#from jetson_inference import detectNet
+#from jetson_utils import videoSource, videoOutput
+import jetson.inference
+import jetson.utils
+
+net = jetson.inference.detectNet("ssd-mobilenet-v2", threshold=0.5)
+
+camera = jetson.utils.videoSource("/home/nvidia/Desktop/humans_0.jpg")
+
+display = jetson.utils.videoOutput("display://0")    # my_video.mp4' for file
+
+while display.IsStreaming(): # main loop will go here
+	img = camera.Capture()
+	
+	if img is None: # capture timeout
+		continue
+
+	detections = net.Detect(img)
+	print(detections[0])
+
+	display.Render(img)
+	display.SetStatus("Object Detection | Network {:.0f} FPS".format(net.GetNetworkFPS()))
